f4fac41893b5416044d1198902171bc6fac87294
Renato "Lond" Cerqueira <renato@lond.com.br>
2018-08-23 19:30:09 +0200

    Use backend from glitch-soc for instance-only toots

--- db/schema.rb
+++ db/schema.rb
@@ -581,6 +581,7 @@ ActiveRecord::Schema.define(version: 2019_02_03_180359) do
     t.bigint "account_id", null: false
     t.bigint "application_id"
     t.bigint "in_reply_to_account_id"
+    t.boolean "local_only"
     t.index ["account_id", "id", "visibility", "updated_at"], name: "index_statuses_20180106", order: { id: :desc }
     t.index ["in_reply_to_account_id"], name: "index_statuses_on_in_reply_to_account_id"
     t.index ["in_reply_to_id"], name: "index_statuses_on_in_reply_to_id"
